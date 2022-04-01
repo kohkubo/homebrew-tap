0ddf2b582d0af2e96d99a229d828436420da9c5d
kohkubo <kohkubo@student.42tokyo.jp>
2022-04-01 19:12:12 +0900

    term3d

---
+++ Formula/term3d.rb
@@ -0,0 +1,10 @@
+class Term3d < Formula
+  desc "3D terminal emulator"
+  homepage ""
+  url "https://github.com/kohkubo/term3d/releases/download/0.0.1/term3d"
+  sha256 "d3522ecbe8b384f02830395bc0c283efb582bb20763a03fff00fa536cb4cb755"
+  license ""
+  def install
+    bin.install "term3d"
+  end
+end
